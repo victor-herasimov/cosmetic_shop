1cb9aaa2f3f610e88d84eff099c67bb3eb434d5c
Viktor <gvs1991.vs@gmail.com>
2026-06-10 08:44:52 +0000

    Create migrations for SiteConfig

--- main/admin.py
+++ main/admin.py
@@ -8,7 +8,7 @@ from .models import SiteConfig
 
 @admin.register(SiteConfig)
 class SiteConfigAdmin(SingletonModelAdmin):
-    list_display = ["title", "short_sescription"]
+    list_display = ["title", "short_description"]
     list_display_links = ["title"]
     fields = ["title", "short_sescription", "slogan", "logo"]
     readonly_fields = ["created", "updated"]
