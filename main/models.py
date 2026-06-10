a713529053ee65511ed10d111dc17736846587dc
Viktor <gvs1991.vs@gmail.com>
2026-06-10 08:43:25 +0000

    Create site config model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from solo.models import SingletonModel
 
-# Create your models here.
+
+class SiteConfig(SingletonModel):
+    title = models.CharField(
+        max_length=255, default="Назва сайту", verbose_name="Назва сайту"
+    )
+    short_description = models.CharField(
+        max_length=512,
+        blank=True,
+        null=True,
+        default=None,
+        verbose_name="Короткий опис",
+    )
+    logo = models.ImageField(
+        blank=True, null=True, upload_to="logo", verbose_name="Логотип"
+    )
+    slogan = models.CharField(
+        max_length=512, null=True, blank=True, default=None, verbose_name="Гасло"
+    )
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Дата створення")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Дата оновлення")
+
+    class Meta:
+        verbose_name: str = "Конфігурація сайту"
+
+    def __str__(self) -> str:
+        return f"{self.title}"
