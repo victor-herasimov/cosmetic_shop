a8ecbce44c54796a5379011a26ff9712bc89e531
Viktor <gvs1991.vs@gmail.com>
2026-06-10 08:42:46 +0000

    Create site config model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from solo.admin import SingletonModelAdmin
+from .models import SiteConfig
 
-# Register your models here.
+
+# admin.site.register(SiteConfig, SingletonModelAdmin)
+
+
+@admin.register(SiteConfig)
+class SiteConfigAdmin(SingletonModelAdmin):
+    list_display = ["title", "short_sescription"]
+    list_display_links = ["title"]
+    fields = ["title", "short_sescription", "slogan", "logo"]
+    readonly_fields = ["created", "updated"]
